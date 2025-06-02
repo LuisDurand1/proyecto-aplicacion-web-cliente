b1015ee7fbd90d5ddd3f17b9d7223318f2236624
ldurand <luis-rodrigo2213@hotmail.com>
2025-06-01 22:30:13 -0300

    adding some features to the cart

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const API_TOKEN= 'patu4rl1RgVOkZQmf.63aefb3d103dd693730bff34df288a03b8a61bb495720383b51761c7f6a909e4'; 
+const API_TOKEN = 'patu4rl1RgVOkZQmf.63aefb3d103dd693730bff34df288a03b8a61bb495720383b51761c7f6a909e4';
 const BASE_ID = 'appbMQJ3qhcx04rtO';
 const API_URL = `https://api.airtable.com/v0/${BASE_ID}/`;
+
+const cartProducts = JSON.parse(localStorage.getItem('cart')) || [];
 
 
 function toggleDropdownForOrder() {
@@ -20,17 +22,17 @@ function toggleDropdownForFilter() {
 
 
 const getProducts = async (container) => {
-   await fetch(API_URL+container.dataset.table, {
+    await fetch(API_URL + container.dataset.table, {
         method: 'GET',
-        headers:{
+        headers: {
             'Authorization': `Bearer ${API_TOKEN}`,
             'Content-Type': 'application/json'
         }
     }).then(res => res.json())
-      .then(data => {
-        mapProducts(data.records, container);
-      })
-      .catch(err => console.error('Error al cargar productos:', err)); 
+        .then(data => {
+            mapProducts(data.records, container);
+        })
+        .catch(err => console.error('Error al cargar productos:', err));
 }
 
 
@@ -57,9 +59,22 @@ function createProductCard(product) {
     divbuyButton.classList.add('buy-button');
     divbuyButton.innerText = 'Comprar';
 
+    divbuyButton.addEventListener('click', () => {
+        const existingProduct = cartProducts.find(p => p.id === product.id);
+        if (existingProduct) {
+            existingProduct.quantity += 1;
+        } else {
+            console.log('Adding product to cart:', product);
+            console.log('Current cart:', cartProducts);
+            cartProducts.push(product);
+            localStorage.setItem('cart', JSON.stringify(cartProducts));
+        }
+    }
+    )
+
 
     divCard.append(divImg, h3, divPrice, divbuyButton);
-   
+
     return divCard;
 }
 
@@ -98,3 +113,67 @@ document.addEventListener('DOMContentLoaded', async () => {
         Array.from(containers).map(container => getProducts(container))
     );
 });
+
+
+function showCart() {
+    const cartIcon = document.getElementById('cart-icon');
+    const cartItems = document.querySelector('.cart-items');
+
+    cartIcon.addEventListener('click', () => {
+        const cartOverlay = document.getElementById('cart-overlay');
+
+        const body = document.querySelector('body');
+        body.style.overflow = 'hidden';
+        cartItems.innerHTML = '';
+        cartOverlay.style.display = 'flex'
+
+        if (cartProducts.length === 0) {
+            const divCont = document.createElement('div');
+            divCont.classList.add('cart-empty');
+            const h2 = document.createElement('h2');
+            h2.innerText = 'El carrito está vacío';
+
+            divCont.appendChild(h2);
+            cartItems.appendChild(divCont);
+        }
+
+    })
+    /* 
+ 
+     cartContainer.innerHTML = '';
+ 
+     if (cartProducts.length === 0) {
+         cartContainer.innerHTML = '<p>No hay productos en el carrito.</p>';
+         return;
+     }
+ 
+     const cartTitle = document.createElement('h2');
+     cartTitle.innerText = 'Carrito de Compras';
+     cartContainer.appendChild(cartTitle);
+ 
+     const cartList = document.createElement('ul');
+ 
+     cartProducts.forEach(product => {
+         const listItem = document.createElement('li');
+         listItem.innerText = `${product.name} - $${product.price} x ${product.quantity}`;
+         cartList.appendChild(listItem);
+     });
+ 
+     cartContainer.appendChild(cartList); */
+}
+
+function closeCart() {
+    const closeCart = document.querySelector('.close-cart');
+    closeCart.addEventListener('click', () => {
+        const cartOverlay = document.getElementById('cart-overlay');
+        const body = document.querySelector('body');
+
+        body.style.overflow = 'auto';
+        cartOverlay.style.display = 'none';
+    })
+}
+
+
+
+showCart();
+closeCart();
